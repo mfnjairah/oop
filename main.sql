ac064c81cd6434ccd7d7787a18de8f4da12e5c88
mfnjairah <jairahtracks@gmail.com>
2024-01-29 20:14:14 +0800

    create queries

---
+++ main.sql
@@ -0,0 +1,38 @@
+-- Create new table
+
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    middle_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    location VARCHAR(100)
+);
+
+-- Insert 6 records
+
+INSERT INTO students (first_name, middle_name, last_name, age, location)
+VALUES
+    (1, 'Juan', 'Blank', 'Cruz', 18, 'Manila'),
+    (2, 'Anne', 'Blank', 'Wall', 20, 'Manila'),
+    (3, 'Theresa', 'Blank', 'Joseph', 21, 'Manila'),
+    (4, 'Issac', 'Blank', 'Gray', 19, 'Laguna'),
+    (5, 'Zack', 'Blank', 'Matthews', 22, 'Marikina'),
+    (6, 'Finn', 'Blank', 'Lam', 25, 'Manila');
+
+-- Update first record
+
+UPDATE students
+SET first_name = 'Ivan', middle_name = 'Ingram', age = 25, last_name = 'Howard'
+WHERE id = 1;
+
+-- Delete last record
+
+DELETE FROM students
+WHERE id = 6;
+
+-- or
+
+DELETE FROM students
+ORDER BY id DESC
+LIMIT 1;
