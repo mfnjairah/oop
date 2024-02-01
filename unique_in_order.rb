aa6f7996442127fd5d81a634cc61723e6a6de9c6
mfnjairah <jairahtracks@gmail.com>
2024-02-01 19:30:32 +0800

    unique in order

---
+++ unique_in_order.rb
@@ -0,0 +1,30 @@
+# Implement the method unique_in_order which takes as argument a sequence and returns a list of items without any elements
+# with the same value next to each other and preserving the original order of elements.
+
+def unique_in_order(sequence)
+    collection = []
+
+    if sequence.is_a?(String)
+        sequence.chars.each do |element|
+            if collection.empty? || collection.last != element
+                collection.push(element)
+            end
+        end
+    elsif sequence.is_a?(Array)
+        sequence.each do |element|
+            if collection.empty? || collection.last != element
+                collection.push(element)
+            end
+        end
+    else
+        puts "Unsupported data type"
+        return
+    end
+
+    collection
+end
+
+# Test cases
+puts unique_in_order('AAAABBBCCDAABBB').inspect  # ['A', 'B', 'C', 'D', 'A', 'B']
+puts unique_in_order('ABBCcAD').inspect           # ['A', 'B', 'C', 'c', 'A', 'D']
+puts unique_in_order([1, 2, 2, 3, 3]).inspect     # [1, 2, 3]
