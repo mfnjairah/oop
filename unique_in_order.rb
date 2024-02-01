dae7519df8f1bac0766eeda98daa2efd36492962
mfnjairah <jairahtracks@gmail.com>
2024-02-01 19:43:23 +0800

    Revert

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -1,18 +1,15 @@
-# Implement the method unique_in_order which takes as argument a sequence and returns a list of items without any elements
-# with the same value next to each other and preserving the original order of elements.
-
 def unique_in_order(sequence)
     collection = []
 
     if sequence.is_a?(String)
         sequence.chars.each do |element|
-            if !collection.include?(element)
+            if collection.empty? || collection.last != element
                 collection.push(element)
             end
         end
     elsif sequence.is_a?(Array)
         sequence.each do |element|
-            if !collection.include?(element)
+            if collection.empty? || collection.last != element
                 collection.push(element)
             end
         end
@@ -25,6 +22,6 @@ def unique_in_order(sequence)
 end
 
 # Test cases
-puts unique_in_order('AAAABBBCCDAABBB').inspect     # ['A', 'B', 'C', 'D', 'A', 'B']
-puts unique_in_order('ABBCcAD').inspect             # ['A', 'B', 'C', 'c', 'A', 'D']
-puts unique_in_order([1, 2, 2, 3, 3]).inspect       # [1, 2, 3]
+puts unique_in_order('AAAABBBCCDAABBB').inspect  # ['A', 'B', 'C', 'D', 'A', 'B']
+puts unique_in_order('ABBCcAD').inspect           # ['A', 'B', 'C', 'c', 'A', 'D']
+puts unique_in_order([1, 2, 2, 3, 3]).inspect     # [1, 2, 3]
